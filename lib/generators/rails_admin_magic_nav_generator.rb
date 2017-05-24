fec0b7c39a92d9c04a77d1a29f58430c2d242b8b
Erik <erik@hhd.com.au>
2017-05-24 15:38:39 +1000

    make better source_root

--- lib/generators/rails_admin_magic_nav_generator.rb
+++ lib/generators/rails_admin_magic_nav_generator.rb
@@ -1,8 +1,8 @@
 class RailsAdminMagicNavGenerator < Rails::Generators::Base
 
-  source_root File.expand_path("../../../config", __FILE__)
+  source_root File.expand_path("../../../", __FILE__)
 
   def create_magic_nav_yml
-    copy_file("magic_nav.yml", "config/rails_admin/magic_nav.yml")
+    copy_file("config/magic_nav.yml", "config/rails_admin/magic_nav.yml")
   end
 end
