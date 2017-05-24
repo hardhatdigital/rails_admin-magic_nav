a1f13672cd352aac7f3ffd2241d74453dcbc67b2
Erik <erik@hhd.com.au>
2017-05-24 16:59:39 +1000

    remove comments

--- app/controllers/magic_nav_controller.rb
+++ app/controllers/magic_nav_controller.rb
@@ -3,8 +3,6 @@ class RailsAdmin::MagicNavController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    # @nav_labels = NavLabel.all
-    # @page_conts = PageContainer.includes(:intro_blocks, :content_blocks)
     @nav = YAML.load_file('./config/rails_admin/magic_nav.yml')
 
     render layout: false
