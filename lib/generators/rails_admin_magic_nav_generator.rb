7af6fa02028fd68b95df8481a1475c23304a2836
Erik <erik@hhd.com.au>
2017-05-24 16:19:38 +1000

    add stylesheet

--- lib/generators/rails_admin_magic_nav_generator.rb
+++ lib/generators/rails_admin_magic_nav_generator.rb
@@ -6,8 +6,13 @@ class RailsAdminMagicNavGenerator < Rails::Generators::Base
     copy_file("config/magic_nav.yml", "config/rails_admin/magic_nav.yml")
   end
 
-  def create_rails_admin_ui_js
+  def create_javascripts
     copy_file("app/assets/javascripts/ui.js", "app/assets/javascripts/rails_admin/custom/ui.js")
   end
+
+  def create_stylesheets
+    copy_file("app/assets/stylesheets/mixins.scss", "app/assets/stylesheets/rails_admin/custom/mixins.scss")
+  end
+  
 
 end
